c68af189d297bcd9fffb20cc241924d9b86aa591
EmperorIrritant <95113377+EmperorIrritant@users.noreply.github.com>
2021-12-29 06:03:58 +0000

    Changes made based on testing results
    - Replaced receiving useragent with all headers
    - Replaced deprecated has_key method with has_attr in bs4, according to warning
    - Fixed wrong variable name (link and css)
    - Fixed requests session left unclosed
    - Fixed passing page content as string instead of bytes for compression

--- webproxy.py
+++ webproxy.py
@@ -1,27 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 import zlib
-
+from urllib.parse import urlparse, urljoin
 from base64 import b64encode
 
 def lambda_handler(event, context):
     page_url = event["request"]["page_url"]
-    useragent = event["request"]["useragent"]
-    headers = {'User-Agent': useragent}
+    headers = event["request"]["headers"]
     session = requests.Session()
     requested_page = session.get(page_url, headers=headers)
-    pagesoup = BeautifulSoup(requested_page.text)
+    pagesoup = BeautifulSoup(requested_page.text, "lxml")
+    parsed_uri = urlparse(page_url)
+    domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+    for form in pagesoup.findAll('form'):
+        if form['action'].startswith('/'):
+            form['action'] = urljoin(domain, form['action'])
     for img in pagesoup.findAll('img'):
-        if img.has_key('src'):
+        if img.has_attr('src'):
+            if img['src'].startswith('/'):
+                img['src'] = urljoin(domain, img['src'])
             image = session.get(img['src'], headers=headers)
             img['src'] = f"data:{image.headers['Content-Type']},base64,{b64encode(image)}"
     for link in pagesoup.findAll('link'):
-        if link.has_key('href'):
-            css = session.get(css['href'], headers=headers)
+        if link['rel'] == "stylesheet" and link.has_attr('href'):
+            if link['href'].startswith('/'):
+                link['href'] = urljoin(domain, link['href'])
+            css = session.get(link['href'], headers=headers)
             pagesoup.head.style.append(css.text)
+        else:
+            link['href'] = ''
     for script in pagesoup.findAll('script'):
-        if script.has_key('src'):
+        if script.has_attr('src'):
+            if script['src'].startswith('/'):
+                script['src'] = urljoin(domain, script['src'])
             js = session.get(script['src'], headers=headers)
             pagesoup.head.script.append(js.text)
+    session.close()
 
-    return b64encode(zlib.compress(str(pagesoup)))
+    return b64encode(zlib.compress(str(pagesoup).encode('utf-8')))
