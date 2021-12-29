e9b2ba276e778963f678da611ac8e94644e3362a
EmperorIrritant <95113377+EmperorIrritant@users.noreply.github.com>
2021-12-29 06:29:51 +0000

    Requests for assets with threads
    - No improvement in time and even some small increase observed

--- webproxy.py
+++ webproxy.py
@@ -3,6 +3,16 @@ from bs4 import BeautifulSoup
 import zlib
 from urllib.parse import urlparse, urljoin
 from base64 import b64encode
+from threading import Thread
+
+threads = []
+
+def get_url_content(url, session, headers, htmlelement):
+    elementcontent = session.get(url, session, headers=headers)
+    if htmlelement.name == 'img':
+        htmlelement['src'] = f"data:{image.headers['Content-Type']},base64,{b64encode(elementcontent)}"
+    else:
+        htmlelement.append(elementcontent)
 
 def lambda_handler(event, context):
     page_url = event["request"]["page_url"]
@@ -19,22 +29,29 @@ def lambda_handler(event, context):
         if img.has_attr('src'):
             if img['src'].startswith('/'):
                 img['src'] = urljoin(domain, img['src'])
-            image = session.get(img['src'], headers=headers)
-            img['src'] = f"data:{image.headers['Content-Type']},base64,{b64encode(image)}"
+            thread = Thread(target=get_url_content, args=(img['src'], session, headers, img))
+            threads.append(thread)
     for link in pagesoup.findAll('link'):
         if link['rel'] == "stylesheet" and link.has_attr('href'):
             if link['href'].startswith('/'):
                 link['href'] = urljoin(domain, link['href'])
-            css = session.get(link['href'], headers=headers)
-            pagesoup.head.style.append(css.text)
+            thread = Thread(target=get_url_content, args=(link['href'], session, headers, pagesoup.head.style))
+            threads.append(thread)
         else:
             link['href'] = ''
     for script in pagesoup.findAll('script'):
         if script.has_attr('src'):
             if script['src'].startswith('/'):
                 script['src'] = urljoin(domain, script['src'])
-            js = session.get(script['src'], headers=headers)
-            pagesoup.head.script.append(js.text)
+            thread = Thread(target=get_url_content, args=(script['src'], session, headers, pagesoup.head.script))
+            threads.append(thread)
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
     session.close()
 
     return b64encode(zlib.compress(str(pagesoup).encode('utf-8')))
